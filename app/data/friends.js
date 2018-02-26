f5d236f2888ea97a16b47859a0d1292029dc732c
alyciamriley <alyciarileym@gmail.com>
2018-02-26 14:29:18 -0600

    getting undefined error for userResponse.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,41 @@
 //create friend array
 
-var friendArray = [
+var friends = [
   {
-    friendName: "Paul Newman",
-    friendPhoto:
+    friend: "Paul Newman",
+    photo:
       "https://drive.google.com/open?id=1EpEjZ8V127xl3znatmOqVA1uawQlIdcx",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
 
   {
-    friendName: "James Dean",
-    friendPhoto:
+    friend: "James Dean",
+    photo:
       "https://drive.google.com/open?id=1LHW4WW6iTthwcxqT-6-A4H3SF2v-0EyZ",
     scores: [1, 3, 5, 2, 5, 4, 2, 3, 4, 1]
   },
 
 
   {
-    friendName: "Marlon Brando",
-    friendPhoto:
+    friend: "Marlon Brando",
+    photo:
       "https://drive.google.com/open?id=1c0oSDTM5huFEqifI0PZq9ca6nrU6GOPf",
     scores: [4, 1, 1, 3, 5, 2, 1, 5, 4, 3]
   },
 
   {
-    friendName: "Marilyn Monroe",
-    friendPhoto:
+    friend: "Marilyn Monroe",
+    photo:
       "https://drive.google.com/open?id=1J5eaCulgs9BctJdtD_kQr1uLCeVqMOc2",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
 
   {
-    friendName: "Lucille Ball",
-    friendPhoto:
+    friend: "Lucille Ball",
+    photo:
       "https://drive.google.com/open?id=1whxAnz0iIG3UXkBmXU7w0Tq6l7-lp3JQ",
     scores: [5, 4, 4, 2, 5, 1, 5, 5, 2, 3]
   },
 ];
 
-module.exports = friendArray;
+module.exports = friends;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,21 +4,55 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendArray = require("../app/data/friends");
+var friends = require("../app/data/friends");
+var path = require('path');
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-        res.json(friendArray);
-    });
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
+  app.post("/api/friends", function(req, res) {
+    var userInput = req.body;
+    console.log("req body = " + JSON.stringify(req.body));
+    console.log("userInput = " + JSON.stringify(userInput));
 
-app.post("/api/friends", function(req, res){
-        friendArray.push(req.body);
-        res.json(true);
-})
+    var userResponses = userInput.scores;
+    console.log("userResponses = " + userResponses);
 
+    //compute best friend match
 
-}
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 1000;
+
+    //examine all existing friends in list
+    for (var i = 0; i < friends.length; i++) {
+      console.log("friend = " + JSON.stringify(friends[i]));
+
+      //compute differences for each question
+
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+      }
+      console.log("diff = " + diff);
+
+      if (diff < totalDifference) {
+        console.log("closest match found = " + diff);
+        console.log("Friend Name = " + friends[i].name);
+        console.log("Friend image = " + friends[i].photo);
+
+        totalDifference = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
+
+    friends.push(userInput);
+    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+  });
+};
