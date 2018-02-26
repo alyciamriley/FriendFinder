d6fe505431091ec73fb10dbc3f49c3fff5a43527
alyciamriley <alyciarileym@gmail.com>
2018-02-26 13:06:18 -0600

    modal works.  dummy data entered.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,41 @@
 //create friend array
 
 var friendArray = [
-    {
-      friendName: "Ahmed",
-      friendPhoto: "ahmed@example.com",
-      score: []
-    
-    }
-  ];
+  {
+    friendName: "Paul Newman",
+    friendPhoto:
+      "https://drive.google.com/open?id=1EpEjZ8V127xl3znatmOqVA1uawQlIdcx",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
 
-  module.exports = friendArray;
-  
+  {
+    friendName: "James Dean",
+    friendPhoto:
+      "https://drive.google.com/open?id=1LHW4WW6iTthwcxqT-6-A4H3SF2v-0EyZ",
+    scores: [1, 3, 5, 2, 5, 4, 2, 3, 4, 1]
+  },
+
+
+  {
+    friendName: "Marlon Brando",
+    friendPhoto:
+      "https://drive.google.com/open?id=1c0oSDTM5huFEqifI0PZq9ca6nrU6GOPf",
+    scores: [4, 1, 1, 3, 5, 2, 1, 5, 4, 3]
+  },
+
+  {
+    friendName: "Marilyn Monroe",
+    friendPhoto:
+      "https://drive.google.com/open?id=1J5eaCulgs9BctJdtD_kQr1uLCeVqMOc2",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+
+  {
+    friendName: "Lucille Ball",
+    friendPhoto:
+      "https://drive.google.com/open?id=1whxAnz0iIG3UXkBmXU7w0Tq6l7-lp3JQ",
+    scores: [5, 4, 4, 2, 5, 1, 5, 5, 2, 3]
+  },
+];
+
+module.exports = friendArray;
