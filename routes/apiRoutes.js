28289dfc8ef09811f398296bdefb7c311a7b5174
alyciamriley <alyciarileym@gmail.com>
2018-02-26 18:27:31 -0600

    css is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,44 +15,44 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
-    var userInput = req.body;
-    console.log("req body = " + JSON.stringify(req.body));
-    console.log("userInput = " + JSON.stringify(userInput));
+  // app.post("/api/friends", function(req, res) {
+  //   var userInput = req.body;
+  //   console.log("req body = " + JSON.stringify(req.body));
+  //   console.log("userInput = " + JSON.stringify(userInput));
 
-    var userResponses = userInput.scores;
-    console.log("userResponses = " + userResponses);
+  //   var userResponses = userInput.scores;
+  //   console.log("userResponses = " + userResponses);
 
-    //compute best friend match
+  //   //compute best friend match
 
-    var matchName = "";
-    var matchImage = "";
-    var totalDifference = 1000;
+  //   var matchName = "";
+  //   var matchImage = "";
+  //   var totalDifference = 1000;
 
-    //examine all existing friends in list
-    for (var i = 0; i < friends.length; i++) {
-      console.log("friend = " + JSON.stringify(friends[i]));
+  //   //examine all existing friends in list
+  //   for (var i = 0; i < friends.length; i++) {
+  //     console.log("friend = " + JSON.stringify(friends[i]));
 
-      //compute differences for each question
+  //     //compute differences for each question
 
-      var diff = 0;
-      for (var j = 0; j < userResponses.length; j++) {
-        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-      }
-      console.log("diff = " + diff);
+  //     var diff = 0;
+  //     for (var j = 0; j < userResponses.length; j++) {
+  //       diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+  //     }
+  //     console.log("diff = " + diff);
 
-      if (diff < totalDifference) {
-        console.log("closest match found = " + diff);
-        console.log("Friend Name = " + friends[i].name);
-        console.log("Friend image = " + friends[i].photo);
+  //     if (diff < totalDifference) {
+  //       console.log("closest match found = " + diff);
+  //       console.log("Friend Name = " + friends[i].name);
+  //       console.log("Friend image = " + friends[i].photo);
 
-        totalDifference = diff;
-        matchName = friends[i].name;
-        matchImage = friends[i].photo;
-      }
-    }
+  //       totalDifference = diff;
+  //       matchName = friends[i].name;
+  //       matchImage = friends[i].photo;
+  //     }
+  //   }
 
-    friends.push(userInput);
-    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-  });
+  //   friends.push(userInput);
+  //   res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+  // });
 };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
 function css(request, response) {
     if (request.url === '..public/css/styles.css') {
       response.writeHead(200, {'Content-type' : 'text/css'});
@@ -23,6 +24,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public')); 
 
 // app.use(function (req, res) {
 //     res.setHeader('Content-Type', 'text/plain')
