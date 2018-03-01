afce40702473b2a8f16c3167fccc1c7cef7b6455
alyciamriley <alyciarileym@gmail.com>
2018-03-01 13:26:03 -0600

    more design tweaks.

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,7 @@
 var friendsArray = [
   {
     name: "Paul Newman",
-    photo: "https://drive.google.com/open?id=1EpEjZ8V127xl3znatmOqVA1uawQlIdcx",
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Paul_Newman_-_1963.jpg/220px-Paul_Newman_-_1963.jpg',
     scores: [
 			'1',
 			'5',
@@ -22,7 +22,7 @@ var friendsArray = [
   
 {
     name: "James Dean",
-    photo: "https://drive.google.com/open?id=1LHW4WW6iTthwcxqT-6-A4H3SF2v-0EyZ",
+    photo: 'https://www.biography.com/.image/t_share/MTE1ODA0OTcxNjAzMTAxMTk3/james-dean-9268866-1-402.jpg',
     scores: [
 			'2',
 			'4',
@@ -39,7 +39,7 @@ var friendsArray = [
 
   {
     name: "Marlon Brando",
-    photo: "https://drive.google.com/open?id=1c0oSDTM5huFEqifI0PZq9ca6nrU6GOPf",
+    photo: 'https://i.guim.co.uk/img/media/a73c230482826b70bef8be0e32c2d6063c6d8bd4/526_1571_3311_4136/master/3311.jpg?w=300&q=55&auto=format&usm=12&fit=max&s=6779eeaf38e9ce18beb69b8440367873',
     scores: [
 			'4',
 			'3',
@@ -55,8 +55,8 @@ var friendsArray = [
   },
 
   {
-    name: "Marilyn Monroe",
-    photo: "https://drive.google.com/open?id=1J5eaCulgs9BctJdtD_kQr1uLCeVqMOc2",
+    name: "Julie Newmar",
+    photo: 'https://13thdimension.com/wp-content/uploads/2015/08/julie-newmar-2.jpg',
     scores: [
 			'4',
 			'3',
@@ -73,11 +73,11 @@ var friendsArray = [
 
   {
     name: "Lucille Ball",
-    photo: "https://drive.google.com/open?id=1whxAnz0iIG3UXkBmXU7w0Tq6l7-lp3JQ",
+    photo: 'http://cdn.skim.gs/images/c_fill,dpr_2.0,f_auto,fl_lossy,h_391,q_auto,w_695/copngiigrhxngbtowszg/facts-about-lucille-ball',
     scores: [
 			'5',
 			'1',
-			'2',
+			'2',	 
 			'1',
 			'3',
 			'4',
